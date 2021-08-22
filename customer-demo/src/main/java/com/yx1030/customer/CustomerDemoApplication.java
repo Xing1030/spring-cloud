2b8528e093aa42c6da893c0f796eb90b3c193db1
Reverso <liusx997@gmail.com>
2021-08-22 23:07:39 +0800

    增加ribbon功能

--- customer-demo/src/main/java/com/yx1030/customer/CustomerDemoApplication.java
+++ customer-demo/src/main/java/com/yx1030/customer/CustomerDemoApplication.java
@@ -1,14 +1,21 @@
 package com.yx1030.customer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+// application.properties中配置的
+@RibbonClients(
+        @RibbonClient(value = "provider-peer")
+)
 public class CustomerDemoApplication {
 
     public static void main(String[] args) {
@@ -20,4 +27,11 @@ public class CustomerDemoApplication {
     public RestTemplate template() {
         return new RestTemplate();
     }
+
+    /*//自定义负载均衡规则
+    @Bean
+    public IRule ribbonRule() {
+        // 自定义规则
+        return new RandomRule();
+    }*/
 }
--- customer-demo/src/main/java/com/yx1030/customer/controller/CustomerController.java
+++ customer-demo/src/main/java/com/yx1030/customer/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.yx1030.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,10 +13,19 @@ import javax.inject.Inject;
 public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("index")
     public Object getIndex(){
         // getForObject("亲求地址",参数类型,"参数");
-        return restTemplate.getForObject("http://PROVIDER-PEER/",String.class,"");
+        //return restTemplate.getForObject("http://PROVIDER-PEER/",String.class,"");
+        // ribbon使用
+        /*ServiceInstance choose = loadBalancerClient.choose("provider-peer");
+        String host = choose.getHost();
+        int port = choose.getPort();
+        return restTemplate.getForObject("http://"+host+":"+port,String.class,"");*/
+        return restTemplate.getForObject("http://provider-peer",String.class,"");
     }
 
 }
